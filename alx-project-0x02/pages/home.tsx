ab9d7d4a0aee587f47e9a8c49af8ec5104ca390f
XpertDude <braodby@gmail.com>
2025-07-22 21:43:18 +0100

    fixing card component

--- alx-project-0x02/pages/home.tsx
+++ alx-project-0x02/pages/home.tsx
@@ -1,11 +1,8 @@
 import { Card } from "@/components/common/Card"
-import { Header } from "@/components/layout/Header"
 
 export default function Home() {
     return <>
         <section>
-            <Header />
-            <h1>This is Home page</h1>
             <Card title="item1" content="hello am her to..." />
         </section>
     </>
