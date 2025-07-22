c714fa63872a8aedf9d9737aa8d6e36f0d35c408
XpertDude <braodby@gmail.com>
2025-07-22 14:28:28 +0100

    second udate on header

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -1,11 +1,13 @@
-export default function Header () {
-    return <>
-    <nav>
-        <h1>logo</h1>
-        <ul>
-            <a href="/home">Home</a>
-            <a href="/about">About</a>
-        </ul>
-    </nav>
-    </>
+import Link from "next/link"
+
+export const Header = () => {
+    return (
+        <>
+            <div>Header</div>
+            <ul>
+                <Link href="/home">Home</Link>
+                <Link href="/about">About</Link>
+                </ul>
+        </>
+    )
 }
