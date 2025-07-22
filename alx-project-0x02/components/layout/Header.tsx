90a05dc097fe64430af6a4fbe7a395ea8524f0f3
XpertDude <braodby@gmail.com>
2025-07-22 14:18:52 +0100

    first commi

---
+++ alx-project-0x02/components/layout/Header.tsx
@@ -0,0 +1,11 @@
+export default function Header () {
+    return <>
+    <nav>
+        <h1>logo</h1>
+        <ul>
+            <a href="/home">Home</a>
+            <a href="/about">About</a>
+        </ul>
+    </nav>
+    </>
+}
---
+++ alx-project-0x02/pages/index.tsx
@@ -0,0 +1,7 @@
+export default function Welcome () {
+  return <>
+  <main>
+    <div>Welcome</div>
+  </main>
+  </>
+}
